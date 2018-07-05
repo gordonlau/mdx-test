cb71552f7a08373a997ea09fe3ba6e10a965992f
Gordon Lau Wai chung <gordonlauwaichung@gmail.com>
2018-07-05 17:44:28 +0800

    First commit

---
+++ src/Hello.md
@@ -0,0 +1,9 @@
+import List from './List.js';
+prop content
+
+# Hello World!!
+This is my first mdx test with a list down there.
+
+<List content={content}/>
+
+## Hello World 2
---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+export default (props)=>(
+    <div>
+        I am in the List Component
+        <div>
+            {props.content}
+        </div>
+    </div>
+)
